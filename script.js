101ad0ca70336674eb5eace1368b8bfd4bb1fe0f
dheacynaraa <dheacynaraa@gmail.com>
2026-05-18 14:09:12 +0700

    Add Data Button Action

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const inputDeadline = $("#inputDeadline");
 const btnAdd = $("#btnAdd");
 const taskList = $("#taskList");
 const btnClear = $("#btnClear");
+
+// Action for Add Data Button
+btnAdd.click(function () {
+  let textTask = inputTask.val();
+
+  let deadlineTask = inputDeadline.val();
+
+  if (textTask === "") {
+    alert("Data must be entered!");
+    return;
+  }
+  
+    if (deadlineTask === "") {
+    alert("Deadline task must be entered!");
+    return;
+  }
+
+  inputTask.val("");
+  
+  let newList = $("<li>");
+  
+});
